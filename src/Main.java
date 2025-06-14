dffa08856aafdaf914424e158656695a69d30359
SEBASTIANULIBARRI <sebastiannahuelulibarri@gmail.com>
2025-06-14 19:47:14 -0300

    Back

--- src/Main.java
+++ src/Main.java
@@ -41,8 +41,9 @@ public class Main {
     // <editor-fold desc="GREEDY">
 
     public static List<Maquina> greedy(List<Maquina> maquinas) {
+
         List<Maquina> solucion = new ArrayList<>();
-        List<Maquina> candidatos = maquinas;
+        List<Maquina> candidatos = new ArrayList<>(maquinas);
         candidatos.sort(Comparator.reverseOrder());
         int contador = 0;
 
@@ -72,12 +73,50 @@ public class Main {
     // </editor-fold>
 
     // <editor-fold desc="BACKTRACKING">
-    // Tu código acá
+    private static int contar(List<Maquina> maquinas) {
+        int contador = 0;
+        for (Maquina m : maquinas) contador += m.getCantidad_piezas();
+
+        return contador;
+    }
+
+    private static void back_recursivo(List<Maquina> maquinas, List<Maquina> sol_parcial, List<Maquina> sol_final) {
+        if (contar(sol_parcial) == CANTIDAD_PIEZAS && (sol_final.isEmpty() || sol_parcial.size() < sol_final.size())) {
+            sol_final.clear();
+            sol_final.addAll(sol_parcial);
+        } else {
+            for (Maquina m : maquinas) {
+                if (contar(sol_parcial) + m.getCantidad_piezas() <= CANTIDAD_PIEZAS) {
+                    sol_parcial.add(m);
+                    back_recursivo(maquinas, sol_parcial, sol_final);
+                    sol_parcial.removeLast();
+                }
+            }
+        }
+
+    }
+
+    public static List<Maquina> back(List<Maquina> maquinas) {
+
+        List<Maquina> sol_parcial = new ArrayList<>();
+        List<Maquina> sol_parcial2 = new ArrayList<>(maquinas);
+        sol_parcial2 = greedy(sol_parcial2);
+
+        List<Maquina> sol_final = (sol_parcial2 == null) ? new ArrayList<>() : sol_parcial2;
+
+        back_recursivo(maquinas, sol_parcial, sol_final);
+        return sol_final;
+
+    }
     // </editor-fold>
 
 
     public static void main(String[] args) {
         List<Maquina> maquinas = leerArchivo("Files/Pieza.csv");
-        greedy(maquinas);
+        List<Maquina> sol_parcial2 = new ArrayList<>(maquinas);
+
+        System.out.println("GREEDY : " + greedy(sol_parcial2));
+        System.out.println("BACK : " + back(maquinas));
+
     }
 }
